57b5a232fb60fb2861c196dd94a6ebd470a36ffa
zubaidr <web.expert1221@gmail.com>
2022-02-23 17:41:14 +0500

    import DataService and Todo and implement OnInit and subscrib data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { DataService } from './data.service';
+import { Todo } from './types';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'API Intgration';
-}
+export class AppComponent implements OnInit {
+
+  title = 'fetching-data';
+  array: Todo[] = [];
+  constructor(public service: DataService) {
+
+  }
+  ngOnInit(): void {
+    this.service.getarraydata().subscribe((value) => {
+      this.array = value;
+      console.warn("value" ,value)
+
+    })
+
+  }
+} 
